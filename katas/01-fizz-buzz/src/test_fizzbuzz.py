966cb9205cdb16d5df0201f8414d92ad1768d32a
Javier Serrano <javier.serrano@clarity.ai>
2021-01-14 11:08:24 +0100

    Case 01

--- katas/01-fizz-buzz/src/test_fizzbuzz.py
+++ katas/01-fizz-buzz/src/test_fizzbuzz.py
@@ -1,6 +1,37 @@
 import unittest
 
+def list_to_100():
+    list_100 = []
+    for i in range(1,101):
+        list_100.append(modify_multiple_of_3_or_5(i))
+    return list_100
+
+def modify_multiple_of_3_or_5(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
 
 class FizzBuzzTest(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        result = list_to_100()
+        self.assertEqual(len(result), 100)
+
+    def test_multiple_of_3(self):
+        result = list_to_100()
+        self.assertEqual(result[2], "Fizz")
+        self.assertEqual(result[95], "Fizz")
+
+    def test_multiple_of_5(self):
+        result = list_to_100()
+        self.assertEqual(result[4], "Buzz")
+        self.assertEqual(result[99], "Buzz")
+
+    def test_multiple_of_3_and_5(self):
+        result = list_to_100()
+        self.assertEqual(result[14], "FizzBuzz")
+        self.assertEqual(result[59], "FizzBuzz")
